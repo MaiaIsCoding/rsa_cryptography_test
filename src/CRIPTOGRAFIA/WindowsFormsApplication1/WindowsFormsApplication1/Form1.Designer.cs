1a800174e36a13bf0752b577c3e42953975b9f53
MaiaIsCoding <61297112+MaiaIsCoding@users.noreply.github.com>
2021-01-18 23:33:33 -0300

    Add files via upload

---
+++ src/CRIPTOGRAFIA/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ToEncrypt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.EncryptedBox = new System.Windows.Forms.RichTextBox();
+            this.ToDecrypt = new System.Windows.Forms.RichTextBox();
+            this.DecryptedBox = new System.Windows.Forms.TextBox();
+            this.PrivateBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ToEncrypt
+            // 
+            this.ToEncrypt.Location = new System.Drawing.Point(110, 19);
+            this.ToEncrypt.Name = "ToEncrypt";
+            this.ToEncrypt.Size = new System.Drawing.Size(343, 20);
+            this.ToEncrypt.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(29, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // EncryptedBox
+            // 
+            this.EncryptedBox.Location = new System.Drawing.Point(110, 57);
+            this.EncryptedBox.Name = "EncryptedBox";
+            this.EncryptedBox.Size = new System.Drawing.Size(343, 76);
+            this.EncryptedBox.TabIndex = 9;
+            this.EncryptedBox.Text = "";
+            // 
+            // ToDecrypt
+            // 
+            this.ToDecrypt.Location = new System.Drawing.Point(110, 153);
+            this.ToDecrypt.Name = "ToDecrypt";
+            this.ToDecrypt.Size = new System.Drawing.Size(343, 73);
+            this.ToDecrypt.TabIndex = 10;
+            this.ToDecrypt.Text = "";
+            // 
+            // DecryptedBox
+            // 
+            this.DecryptedBox.Location = new System.Drawing.Point(110, 337);
+            this.DecryptedBox.Name = "DecryptedBox";
+            this.DecryptedBox.Size = new System.Drawing.Size(343, 20);
+            this.DecryptedBox.TabIndex = 0;
+            // 
+            // PrivateBox
+            // 
+            this.PrivateBox.Location = new System.Drawing.Point(110, 245);
+            this.PrivateBox.Name = "PrivateBox";
+            this.PrivateBox.Size = new System.Drawing.Size(343, 73);
+            this.PrivateBox.TabIndex = 11;
+            this.PrivateBox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(261, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Texto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(234, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Texto Encriptado";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(234, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Texto Encriptado";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(234, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Chave Privada";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(234, 321);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Texto Decifrado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 402);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PrivateBox);
+            this.Controls.Add(this.ToDecrypt);
+            this.Controls.Add(this.EncryptedBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ToEncrypt);
+            this.Controls.Add(this.DecryptedBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ToEncrypt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox EncryptedBox;
+        private System.Windows.Forms.RichTextBox ToDecrypt;
+        private System.Windows.Forms.TextBox DecryptedBox;
+        private System.Windows.Forms.RichTextBox PrivateBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ src/CRIPTOGRAFIA/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static public byte[] Encryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+                try
+                {
+                    byte[] encryptedData;
+                    using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                    {
+                        
+                        RSA.ImportParameters(RSAKey);
+                        encryptedData = RSA.Encrypt(Data, DoOAEPPadding);
+                    }
+                    return encryptedData;
+                 }
+                    catch (CryptographicException e)
+                    {
+                    Console.WriteLine(e.Message);
+                    return null;
+                    }
+        }
+        static public byte[] Decryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+                try
+                {
+                    byte[] decryptedData;
+                    using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                    {
+                        RSA.ImportParameters(RSAKey);
+                        decryptedData = RSA.Decrypt(Data, DoOAEPPadding);
+                    }
+                    return decryptedData; 
+                }
+                catch (CryptographicException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return null;
+                }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var publicKey = "<RSAKeyValue><Modulus>21wEnTU+mcD2w0Lfo1Gv4rtcSWsQJQTNa6gio05AOkV/Er9w3Y13Ddo5wGtjJ19402S71HUeN0vbKILLJdRSES5MHSdJPSVrOqdrll/vLXxDxWs/U0UT1c8u6k/Ogx9hTtZxYwoeYqdhDblof3E75d9n2F0Zvf6iTb4cI7j6fMs=</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(publicKey);
+            byte[] ToEncryptbyte = System.Text.Encoding.Unicode.GetBytes(ToEncrypt.Text);
+            byte[] encryptedData = rsa.Encrypt(ToEncryptbyte, true);
+            EncryptedBox.Text = System.Convert.ToBase64String(encryptedData);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //var privateKey = "<RSAKeyValue><Modulus>21wEnTU+mcD2w0Lfo1Gv4rtcSWsQJQTNa6gio05AOkV/Er9w3Y13Ddo5wGtjJ19402S71HUeN0vbKILLJdRSES5MHSdJPSVrOqdrll/vLXxDxWs/U0UT1c8u6k/Ogx9hTtZxYwoeYqdhDblof3E75d9n2F0Zvf6iTb4cI7j6fMs=</Modulus><Exponent>AQAB</Exponent><P>/aULPE6jd5IkwtWXmReyMUhmI/nfwfkQSyl7tsg2PKdpcxk4mpPZUdEQhHQLvE84w2DhTyYkPHCtq/mMKE3MHw==</P><Q>3WV46X9Arg2l9cxb67KVlNVXyCqc/w+LWt/tbhLJvV2xCF/0rWKPsBJ9MC6cquaqNPxWWEav8RAVbmmGrJt51Q==</Q><DP>8TuZFgBMpBoQcGUoS2goB4st6aVq1FcG0hVgHhUI0GMAfYFNPmbDV3cY2IBt8Oj/uYJYhyhlaj5YTqmGTYbATQ==</DP><DQ>FIoVbZQgrAUYIHWVEYi/187zFd7eMct/Yi7kGBImJStMATrluDAspGkStCWe4zwDDmdam1XzfKnBUzz3AYxrAQ==</DQ><InverseQ>QPU3Tmt8nznSgYZ+5jUo9E0SfjiTu435ihANiHqqjasaUNvOHKumqzuBZ8NRtkUhS6dsOEb8A2ODvy7KswUxyA==</InverseQ><D>cgoRoAUpSVfHMdYXW9nA3dfX75dIamZnwPtFHq80ttagbIe4ToYYCcyUz5NElhiNQSESgS5uCgNWqWXt5PnPu4XmCXx6utco1UVH8HGLahzbAnSy6Cj3iUIQ7Gj+9gQ7PkC434HTtHazmxVgIR5l56ZjoQ8yGNCPZnsdYEmhJWk=</D></RSAKeyValue>";
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(PrivateBox.Text);
+            byte[] ToDecryptbyte = System.Convert.FromBase64String(ToDecrypt.Text);
+            byte[] decryptedData = rsa.Decrypt(ToDecryptbyte, true);
+            DecryptedBox.Text = System.Text.Encoding.Unicode.GetString(decryptedData);
+        }
+    }
+}
